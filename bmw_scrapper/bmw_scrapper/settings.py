1c21624d782c74416600f2d604dc0df3af89b3b5
nomnng <denis.rbn0@gmail.com>
2026-05-31 00:29:14 +0300

    Added downloader middleware for choosing random user agent for each request

--- bmw_scrapper/bmw_scrapper/settings.py
+++ bmw_scrapper/bmw_scrapper/settings.py
@@ -46,9 +46,9 @@ COOKIES_ENABLED = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "bmw_scrapper.middlewares.BmwScrapperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "bmw_scrapper.middlewares.RandomUserAgentDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
